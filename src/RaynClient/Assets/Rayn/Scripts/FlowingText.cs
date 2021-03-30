d95e625290dda51a8e6b1e5068e2dcc2a8c46177
nenoNaninu <neno@melonpan.pro>
2021-03-30 22:43:02 +0900

    ????

--- src/RaynClient/Assets/Rayn/Scripts/FlowingText.cs
+++ src/RaynClient/Assets/Rayn/Scripts/FlowingText.cs
@@ -1,15 +1,19 @@
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ScreenOverwriter
 {
+    // アンカーがcanvasの中心にあること想定して書いてる。
     public class FlowingText : MonoBehaviour
     {
         private string _text;
         private TextMeshProUGUI _textMesh;
         private Canvas _canvas;
         private RectTransform _rectTransform;
+        private CanvasScaler _canvasScaler;
+        private Vector2 _canvasResolution;
         private Vector2 _textSize;
         private float _flowingTimeSpan = 0.5f;
         private float _flowingSpeedPerSec;
@@ -18,31 +22,32 @@ namespace ScreenOverwriter
         {
             _textMesh ??= this.GetComponent<TextMeshProUGUI>();
             _rectTransform ??= this.GetComponent<RectTransform>();
+            _canvasScaler ??= canvas.GetComponent<CanvasScaler>();
 
-            var displaySize = canvas.renderingDisplaySize;
+            _canvasResolution = _canvasScaler.referenceResolution;
 
             _canvas = canvas;
             _flowingTimeSpan = flowingTimeSpan;
 
-            _flowingSpeedPerSec = displaySize.x / _flowingTimeSpan;            
+            _flowingSpeedPerSec = _canvasResolution.x / _flowingTimeSpan;            
 
             _text = text;
             _textMesh.text = text;
             _textSize = new Vector2(_textMesh.preferredWidth, _textMesh.preferredHeight);
 
-            _rectTransform.localPosition = new Vector3(displaySize.x / 2 + _textSize.x / 2, Random.Range(-displaySize.y / 2f, displaySize.y / 2f), 0);
+            _rectTransform.anchoredPosition = new Vector3(_canvasResolution.x / 2 + _textSize.x / 2, Random.Range(-_canvasResolution.y / 2f, _canvasResolution.y / 2f), 0);
         }
 
         public async UniTask PlayAnimation()
         {
             while (true)
             {
-                if (_rectTransform.localPosition.x < -_canvas.renderingDisplaySize.x / 2 - _textSize.x / 2)
+                if (_rectTransform.position.x < -_canvasResolution.x / 2 - _textSize.x / 2)
                 {
                     return;
                 }
 
-                _rectTransform.localPosition += new Vector3(-_flowingSpeedPerSec * Time.deltaTime, 0, 0);
+                _rectTransform.anchoredPosition += new Vector2(-_flowingSpeedPerSec * Time.deltaTime, 0);
                 await UniTask.NextFrame(PlayerLoopTiming.Update);
             }
         }
