d4540fc20a4d1beaa7666e7eb90f3d14a79f673d
nenoNaninu <neno@melonpan.pro>
2021-09-09 16:55:52 +0900

    wip

--- src/RaynServer/SqlCommands/command2.sql
+++ src/RaynServer/SqlCommands/command2.sql
@@ -1,15 +1,17 @@
-create table rayn_db.google_account(
-	Identifier text not null primary key,
-    UserId binary(16) not null ,
-	Email text not null,
-);
-
-
 create table rayn_db.account(
-    UserId binary(16) not null primary key,
+	UserId binary(16) not null primary key,
 	Email text not null,
 	LinkToGoogle boolean not null,
 );
 
-alter table rayn_db.threads
-add AuthorID binary(16) null;
+create table rayn_db.google_account(
+	Identifier text not null primary key,
+	UserId binary(16) not null ,
+	Email text not null,
+);
+
+alter table rayn_db.threads add(
+	AuthorID binary(16) not null,
+	CreatedDate datetime not null,
+	DateOffset time not null,
+);
