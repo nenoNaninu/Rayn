07705f3eafe9fdc88b0abd2e861be56eab7ed2f1
nenoNaninu <neno@melonpan.pro>
2021-11-18 03:18:48 +0900

    refactor

--- src/RaynServer/Rayn/Services/Url/UrlUtility.cs
+++ src/RaynServer/Rayn/Services/Url/UrlUtility.cs
@@ -8,6 +8,7 @@ public static class UrlUtility
 {
     public static string ThreadUrl(string hostDomain, Guid threadId)
         => $"https://{hostDomain}/{ControllerRoute.Value<ThreadRoomController>()}?threadId={threadId.ToString()}";
+
     public static string StreamerUrl(string hostDomain, Guid threadId, Guid ownerId)
         => $"https://{hostDomain}/{ControllerRoute.Value<ThreadRoomController>()}/{nameof(ThreadRoomController.Streamer)}?threadId={threadId.ToString()}&ownerId={ownerId.ToString()}";
 }
